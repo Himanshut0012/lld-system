73b24b0018ee5b56bbc559788c45deb271e576a0
Himanshut0012 <himanshut0012@gmail.com>
2024-05-28 17:45:58 +0530

    tictactoe game

---
+++ TicTacToe/src/main/java/org/example/model/PlayingPiece.java
@@ -0,0 +1,10 @@
+package org.example.model;
+
+public class PlayingPiece {
+
+    public PieceType pieceType;
+
+    public PlayingPiece(PieceType pieceType) {
+        this.pieceType = pieceType;
+    }
+}
